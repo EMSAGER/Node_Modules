2e9c559a112bf3e37f65ce4b0e84cff4a64abddb
Emily <emsager7@gmail.com>
2024-06-26 13:17:03 -0700

    step 2

--- step1.js
+++ step1.js
@@ -8,7 +8,7 @@ const process = require('process');
 function cat(path){
     fs.readFile(path, 'utf8', function(err, data) {
         if(err) {
-            console.log(`Error reading ${path}: ${err}`);
+            console.error(`Error reading ${path}: ${err}`);
             process.exit(1);
         }
         console.log(data);
